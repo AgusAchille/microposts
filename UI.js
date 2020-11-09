a19b98015c7ce5f5a9fd6d484e7d51edd0b62f7a
Agustín Achille <agustin.achille@bairesdev.com>
2020-11-08 21:42:08 -0300

    Add and remove features added

--- UI.js
+++ UI.js
@@ -1,8 +1,9 @@
-const post = document.querySelector('#posts');
+export const postsUI = document.querySelector('#posts');
 const title = document.querySelector('#title');
 const body = document.querySelector('#body');
 const idInput = document.querySelector('#id');
-const postSubmit = document.querySelector('.post-submit');
+export const postSubmit = document.querySelector('.post-submit');
+const postsContainer = document.querySelector('.posts-container');
 const forState = 'add';
 
 export function showPosts(posts) {
@@ -14,10 +15,10 @@ export function showPosts(posts) {
                 <div class="card-body">
                     <h4 class="card-title">${post.title}</h4>
                     <p class="card-text">${post.body}</p>
-                    <a href="#" class="edit card-link data.id="${post.id}">
+                    <a href="#" class="edit card-link" data-id="${post.id}">
                         <i class="fa fa-pencil"></i>
                     </a>
-                    <a href="#" class="delete card-link data.id="${post.id}">
+                    <a href="#" class="delete card-link" data-id="${post.id}">
                         <i class="fa fa-remove"></i>
                     </a>
                 </div>
@@ -25,5 +26,27 @@ export function showPosts(posts) {
         
         }
 
-    post.innerHTML = output;
+    postsUI.innerHTML = output;
+}
+
+export function getInputData(){
+    return {
+        title: title.value.trim(),
+        body: body.value.trim()
     }
+}
+
+export function clearInputs(){
+    title.value = '';
+    body.value = '';
+}
+
+export function showAlert(message, className){
+    const div = document.createElement('div');
+    div.className = className;
+    div.textContent = message;
+
+    postsContainer.insertBefore(div, postsUI);
+    
+    setTimeout(() => {div.remove()}, 3000);
+}
--- app.js
+++ app.js
@@ -1,10 +1,48 @@
 import  EasyHTTP from './easyHTTP.js'
 import * as UI from './UI.js'
 
-document.addEventListener('DOMContentLoaded', getPost);
+let posts = [];
 
-function getPost(){
-    EasyHTTP.get('http://localhost:3000/posts')
-        .then((data) => UI.showPosts(data))
-        .catch((error) => console.log(error));
+document.addEventListener('DOMContentLoaded', getPost);
+UI.postSubmit.addEventListener('click', submitPost);
+UI.postsUI.addEventListener('click', removePost);
+
+async function getPost(){
+    try {
+        const data = await EasyHTTP.get('http://localhost:3000/posts');
+        posts = [...data];
+        UI.showPosts(data);
+    }
+    catch(error){console.log(error)};
+}
+
+function submitPost(){
+    const post = UI.getInputData();
+    
+    if(post.body && post.title){
+        EasyHTTP.post('http://localhost:3000/posts', post)
+            .then(() => {
+                getPost();
+                UI.clearInputs();
+                UI.showAlert('Post added', 'alert alert-success text-center');
+            })
+            .catch(error => console.log(error));
+    }
+}
+
+function removePost(e){
+    e.preventDefault();
+    if(e.target.parentElement.classList.contains('delete')){
+        const id = e.target.parentElement.dataset.id;
+
+        EasyHTTP.delete(`http://localhost:3000/posts/${id}`)
+        .then(() => {
+            getPost();
+            UI.showAlert('Post Deleted', 'alert alert-success text-center');
+        })
+        .catch(error => console.log(error));
+
+
+        e.target.parentElement.parentElement.parentElement.remove();
+    }
 }
